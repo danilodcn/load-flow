8756c5c0880229b31835a9ab697092a7aad6c1b8
Danilo Nascimento <daconnas.dcn@gmail.com>
2023-03-25 17:46:40 -0300

    criando testes para a função de geração de posições aleatórias

--- pso/pso.py
+++ pso/pso.py
@@ -6,16 +6,30 @@ DIMENSAO = 3
 N_INDIVIDUOS = 200
 
 
-def random_population(population: pd.DataFrame) -> pd.DataFrame:
+def random_positions(population: pd.DataFrame) -> pd.DataFrame:
     """
-    funcao
+    Gera posições e velocidades aleatórias para uma data população
+    Examples:
+        >>> p = new_population()
+        >>> r = random_positions(p)
+        >>> len(r)
+        2
+        >>> positions, speeds = r
+        >>> len(positions) == N_INDIVIDUOS
+        True
+        >>> positions.size == N_INDIVIDUOS * DIMENSAO
+        True
+        >>> len(speeds) == N_INDIVIDUOS
+        True
+        >>> speeds.size == N_INDIVIDUOS * DIMENSAO
+        True
     """
-    size = (3, 200)
+    size = (200, 3)
 
-    posicoes = np.random.uniform(0, high=NUMERO_BARRAS, size=size)
-    velocidades = np.random.uniform(size=size)
+    positions = np.random.uniform(0, high=NUMERO_BARRAS, size=size)
+    speeds = np.random.uniform(size=size)
 
-    return posicoes, velocidades
+    return positions, speeds
 
 
 def new_population() -> pd.DataFrame:
@@ -26,11 +40,13 @@ def new_population() -> pd.DataFrame:
      - position: posição da partícula no espaço
      - best_position: melhor posição da partícula
      - objective: valor da função objetivo
-    >>> p = new_population()
-    >>> isinstance(p, pd.DataFrame)
-    True
-    >>> p.empty
-    True
+
+    Examples:
+        >>> p = new_population()
+        >>> isinstance(p, pd.DataFrame)
+        True
+        >>> p.empty
+        True
     """
     population = pd.DataFrame(
         columns=["speed", "position", "best_position", "objective"],
